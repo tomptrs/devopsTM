6344ccb67382d9cbefe409806a181473257418ad
tom_ptrs@yahoo.com <tom_ptrs@yahoo.com>
2020-02-16 18:23:44 +0100

    start project

---
+++ DevopsTM/DevopsTM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevopsTM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
